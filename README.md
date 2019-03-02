6fe1f0fde300612c56323a2785d121b1892e2b6c
Saif Hassan <saiftheboss7@gmail.com>
2019-03-02 17:03:46 +0600

    v1

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # swappa_automatic_price_notification_email
+
+SwappaFetcher fetches price and sends you email based on the price you set.
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install foobar.
+
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+
+Sign into sendgrid to get the api key and replace the api key.
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ Swappa.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3.7
+
+from bs4 import BeautifulSoup
+import requests
+import sendgrid
+from sendgrid.helpers.mail import *
+
+
+sg = sendgrid.SendGridAPIClient(apikey='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')
+from_email = Email("saiftheboss7@gmail.com")
+to_email = Email("saiftheboss7@gmail.com")
+subject = "Swappa Found"
+content = Content("text/plain", "New Product Found at Swappa")
+mail = Mail(from_email, subject, to_email, content)
+
+
+
+
+
+price_list = []
+
+hasReview= True
+
+url='http://swappa.com/mobile/buy/apple-iphone-7/unlocked'
+headers = {'user-agent': 'my-app/0.0.1'}
+source= requests.get(url, headers=headers).text
+
+url_parse= BeautifulSoup(source, 'lxml')
+
+
+
+
+
+for content in url_parse.find_all('span', class_='price'):
+    parse = content.text
+    parse_split = parse.split('$')[1]
+    price_list.append(parse_split)
+
+
+
+
+for i in price_list:
+	if int(i) < 250:
+		print("I am ok")
+		print(i)
+		response = sg.client.mail.send.post(request_body=mail.get())
+		print(response.status_code)
+		print(response.body)
+		print(response.headers)
+		break
